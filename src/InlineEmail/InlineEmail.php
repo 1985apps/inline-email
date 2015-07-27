dce86019cb30e3c56c3dbaeab985fd6056c5e9a6
Prakash Raman <prakash.raman.ka@gmail.com>
2015-07-27 20:37:15 +0530

    pretty much done :)

--- src/InlineEmail/InlineEmail.php
+++ src/InlineEmail/InlineEmail.php
@@ -4,8 +4,32 @@ namespace InlineEmail;
 
 class InlineEmail {
 
-	public static function test(){
-		var_dump("this is it");
+	public $style_path;
+	public $style = [];
+
+	function __construct($style_path){
+		$this->style_path = $style_path;
+		$this->style = include $style_path;
+	}
+
+	public function get($name){
+		if(isset($this->style[$name]))
+			return $this->style[$name];
+		return "";
+	}
+
+	/**
+	* Renders the style tag along with the inline css determined by the "names" passed.
+	*
+	* @param  array $names      Arrays of names
+	* @return void
+	*/
+	public function style($names = []){
+		$values = [];
+		foreach($names as $name)
+			$values[] = $this->get($name);
+
+		echo "style=\"". join("; ", $values) ."\"";
 	}
 
 }
